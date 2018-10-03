66330ba783921931b068d356d3eec5909d60c1f1
Christian Åberg <christian.aberg@live.com>
2018-10-03 12:10:41 +0200

    scroll

--- src/components/Prompt.js
+++ src/components/Prompt.js
@@ -35,7 +35,7 @@ class TerminalPrompt extends Component {
 		this.setState({
 			InputValue: input.value,
 			CommandHistoryPointer: -1
-		}, () => { window.scrollTo(0,document.body.scrollHeight); });
+		}, () => { input.scrollIntoView(); });
 
 		// Calculating suggester position
 		var iX = 0;
