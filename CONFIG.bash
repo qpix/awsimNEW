5910fb982bd03730ccd95e4f4009e82fdd60ca24
Christian Åberg <christian.aberg@live.com>
2018-10-03 12:53:53 +0200

    cool

--- CONFIG.bash
+++ CONFIG.bash
@@ -2,6 +2,9 @@
 AWS_ACCESS_KEY_ID=
 AWS_SECRET_ACCESS_KEY=
 
+# REGION #
+AWS_DEFAULT_REGION=eu-west-1
+
 # COMMANDS #
 aws ec2 describe-instances
 aws dynamodb list-tables
--- SETUP.bash
+++ SETUP.bash
@@ -63,4 +63,5 @@ rm -f tmp.txt
 
 export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
+export AWS_DEFAULT_REGION
 source ../CONFIG.bash
