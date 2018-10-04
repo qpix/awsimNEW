7e66fc1b8f0a800a004c9a8bfdbf42d46253c93a
Christian Åberg <christian.aberg@live.com>
2018-10-04 18:01:56 +0200

    nice

--- config.sh
+++ config.sh
@@ -1,10 +1,9 @@
 # AWS CONFIGURATION #
 AWS_ACCESS_KEY_ID=
 AWS_SECRET_ACCESS_KEY=
-AWS_DEFAULT_REGION=eu-west-1
+AWS_DEFAULT_REGION=us-east-1
 
 # AWS COMMANDS #
 aws ec2 describe-instances
 aws dynamodb list-tables
-aws dynamodb describe-table --table-name MyDB
-aws dynamodb describe-table --table-name MyTable
+aws dynamodb describe-table --table-name ExampleTable
